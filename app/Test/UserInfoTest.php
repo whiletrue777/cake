3188c21180eeee09e1bf1fd3687de1df386fdca4
GOUDA <4line.waon@gmail.com>
2015-09-03 08:13:03 +0000

    test2

--- app/Test/UserInfoTest.php
+++ app/Test/UserInfoTest.php
@@ -4,6 +4,7 @@ class UserInfoTest extends PHPUnit_Framework_TestCase {
 	public function test_setUp(){
 		$this->object = new UserInfo();
 	}
+
 	public function test_verify(){
 	//	$this->assertEquals("UserInfo", $this->UserInfo->$name);
 	}
