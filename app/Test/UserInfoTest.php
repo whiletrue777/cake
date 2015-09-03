dd3a2204c362e89e9b89b90503dd909562d105bd
GOUDA <4line.waon@gmail.com>
2015-09-03 08:52:28 +0000

    test4

--- app/Test/UserInfoTest.php
+++ app/Test/UserInfoTest.php
@@ -2,10 +2,11 @@
 require __DIR__ . '/../Model/UserInfo.php';
 class UserInfoTest extends PHPUnit_Framework_TestCase {
 	public function test_setUp(){
-		$this->object = new UserInfo();
+		$sut = new UserInfo();
+	$this->assertEquals("UserInfo",  get_class($sut));
+	$this->assertEquals("UserInfo", $sut->name);
 	}
 
 	public function test_verify(){
-	//	$this->assertEquals("UserInfo", $this->UserInfo->$name);
 	}
 }
